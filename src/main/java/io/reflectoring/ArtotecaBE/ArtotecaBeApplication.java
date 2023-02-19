4a7ca4fe35a0fc4a11cd1b1e2df626cbcb4b56eb
alex.bartoli@outlook.com <alex.bartoli@outlook.com>
2023-02-19 16:11:12 +0100

    Initial commit

---
+++ src/main/java/io/reflectoring/ArtotecaBE/ArtotecaBeApplication.java
@@ -0,0 +1,13 @@
+package io.reflectoring.ArtotecaBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtotecaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtotecaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/reflectoring/ArtotecaBE/ArtotecaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reflectoring.ArtotecaBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtotecaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
